d0beb58e6ae7e842f1b37e3942876c35e7b0f97e
Mike Harris <mharris717@gmail.com>
2009-12-17 10:13:01 -0500

    require activesupport

--- lib/mongo_persist.rb
+++ lib/mongo_persist.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'mongo'
-require 'activesupport'
+require 'active_support'
 require 'fattr'
 %w(util core_ext array_ext hash_ext base mongo_ext).each { |x| require File.dirname(__FILE__) + "/mongo_persist/#{x}" }
 require 'andand'
